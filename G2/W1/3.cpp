3485b79a8a47efc673e7eb5c7932debb885445c9
Beisenbek Baisakov <bsnbk@Beisenbeks-MacBook-Pro.local>
2017-08-23 14:49:30 +0600

    samples are updated

---
+++ G2/W1/3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+	//возраст человека
+	int age;
+	
+	int a;
+
+	a = 10;
+
+	cout << a * a << endl;
+
+	//int b = a * a;
+	//cout << b << endl;
+
+	return 0;
+}
---
+++ G2/W1/4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+	int a,b;
+
+	a = 10;
+	b = 12;
+
+	cout << a + b << endl;
+
+	return 0;
+}
