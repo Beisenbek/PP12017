403aeb616a4fe43289f29f5670afc1836340d856
Beisenbek Baisakov <beysenbek@gmail.com>
2017-08-26 15:18:22 +0600

    added 4th group

---
+++ W1/G4/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+//main function of program
+int main(){
+
+	//cout << "Hello, world!" <<endl;
+	cout << "Hello, world!\n";
+	/*
+	sdf
+	sdf
+	sdf
+	sdf
+	*/
+	return 0;	
+}
