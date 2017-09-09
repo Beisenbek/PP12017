718033ff16540790af8d0dbd6480ec601c38e2ea
Beisenbek <beysenbek@gmail.com>
2017-09-09 17:54:37 +0600

    'upd'

---
+++ W3/G4/294.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+	int a,b,c;
+
+	cin >> a >> b >> c;
+
+	//cout << max(a,max(b,c));	
+
+	if(a >= b && a >=c) cout << a;
+	else if(b >= a && b >=c) cout << b;
+	else if(c >= b && c >=a) cout << c;
+
+	return 0;
+}
