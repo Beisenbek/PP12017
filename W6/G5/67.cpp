119efa31cfc3bbf57e1be86c8c821d00216144ef
Beisenbek Baisakov <beysenbek@gmail.com>
2017-09-28 11:07:36 +0600

    upd

---
+++ W6/G5/67.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	freopen("input.txt","r",stdin);
+
+	int n;
+	cin >> n;
+
+	int a[n];
+
+	for(int i = 0; i < n; ++i){
+		cin >> a[i];
+	}
+
+
+	bool found = false;
+
+	for(int i = 0; i < n - 1; i++){
+		if(a[i] * a[i+1] > 0){
+			found = true;
+			cout << "YES";
+			break;
+		}
+	}
+
+	if(found == false) cout << "NO";
+
+	cout << endl;
+
+	return 0;
+}
